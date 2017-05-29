257b7c32183389da7410b32575f57c8e9d9c117e
Noam Chitayat <noam@ookla.com>
2017-05-28 22:08:39 -0700

    Fix broken imports in index.js
    I created PR #5 by cherry-picking a commit from PR #4. However,
    that also brought over the removal of index.js ES6 usage, which
    we did NOT want.
    Restoring the usage of 'import' fixes the app.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-var ReactDOM = require('react-dom');
-var App = require('./App');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
