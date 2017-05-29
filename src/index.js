8ed659c39348690b7e4d6e57b766df3c6fe90151
Noam Chitayat <noam@ookla.com>
2017-05-28 21:39:53 -0700

    Removing ES6-reliant boilerplate code
    The React bootstrapping script, create-react-app, does a whole lot more
    than just create a simple HTML page and JS file for React. It actually
    also does things like setting up a file watcher for auto-updates. In
    the interests of time, I'm removing registerServiceWorker.js (the file
    watcher) since it uses a ton of ES6 and otherwise isn't really helpful
    for teaching React.
    
    Also greatly simplifying the actual HTML file.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+var ReactDOM = require('react-dom');
+var App = require('./App');
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
